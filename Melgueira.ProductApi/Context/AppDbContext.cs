0f6a38c8fdb71392777befcd2ae7ac3be5bba665
RMLemos <143395332+RMLemos@users.noreply.github.com>
2024-05-02 12:36:32 +0100

    Models, AppDb Context

--- Melgueira.ProductApi/Context/AppDbContext.cs
+++ Melgueira.ProductApi/Context/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Melgueira.ProductApi.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
 
 namespace Melgueira.ProductApi.Context;
 
@@ -8,4 +9,36 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
+
+    //Fluent API
+
+    protected override void OnModelCreating(ModelBuilder mBuilder)
+    {
+        //category
+        mBuilder.Entity<Category>().HasKey(c => c.CategoryId);
+        mBuilder.Entity<Category>().Property(c => c.CategoryName).HasMaxLength(100).IsRequired();
+
+        //product
+        mBuilder.Entity<Product>().HasKey(c => c.ProductId);
+        mBuilder.Entity<Product>().Property(p => p.ProductName).HasMaxLength(100).IsRequired();
+        mBuilder.Entity<Product>().Property(p => p.Description).HasMaxLength(255).IsRequired();
+        mBuilder.Entity<Product>().Property(p => p.ImageURL).HasMaxLength(255).IsRequired();
+        mBuilder.Entity<Product>().Property(p => p.Price).HasPrecision(12, 2).IsRequired();
+        mBuilder.Entity<Category>().HasMany(g => g.Products).WithOne(c => c.Category).IsRequired().OnDelete(DeleteBehavior.Cascade);
+
+        mBuilder.Entity<Category>().HasData(
+            new Category
+            {
+                CategoryId = 1,
+                CategoryName = "Honey",
+            },
+
+            new Category
+            {
+                CategoryId = 2,
+                CategoryName = "Beverage",
+            }
+      );
+    }
 }
+
---
+++ Melgueira.ProductApi/Migrations/20240502105709_SeedProducts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Melgueira.ProductApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Products(ProductName, Description, Price, ImageURL, Stock, CreatedAt, CategoryId) " +
+                          "Values('Honey','100% pure, natural, ethical and local honey.',7.00,'honey.jpg',10,NOW(),1)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("delete from Products");
+        }
+    }
+}
